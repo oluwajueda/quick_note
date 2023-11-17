5dc8aa8d0c5ca800e1224a9ed7efd58ba0eee863
oluwajueda <oluwajuedaloade@gmail.com>
2023-11-17 09:30:52 +0100

    add boostrap

--- server/controllers/mainController.js
+++ server/controllers/mainController.js
@@ -13,3 +13,13 @@ exports.homepage = async (req, res) => {
         res.render("index", locals);
   
 }
+
+exports.about = async (req, res) => {
+   
+    const locals = {
+        title: "About- NodeJs Notes",
+         description: "Free NodeJs Notes App"
+    }
+    res.render("about", locals);
+
+}
